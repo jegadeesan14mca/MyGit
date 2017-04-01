6256c6072664b09588de4fcd164de92df08c73b7
jegadeesan_s <jegadeesan14mca@gmail.com>
2017-04-01 16:28:48 +0530

    Jegadeesan | Successfully Created Login Page

--- app/src/main/java/com/example/jegadish/mygit/MainActivity.java
+++ app/src/main/java/com/example/jegadish/mygit/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.jegadish.mygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText userName, password;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = (EditText) findViewById(R.id.user_name);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                Toast.makeText(this, "Login Button Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
